2301ad453b27f77275d2c4ad8b31d85d3f1b1616
CarlosJaBr <carjabo12@gmail.com>
2023-10-11 16:04:23 -0500

    feat_add_model_classes

---
+++ src/main/java/com/edu/icesi/LibraryManagement/persistence/model/Author.java
@@ -0,0 +1,16 @@
+package com.edu.icesi.LibraryManagement.persistence.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Author {
+
+    private Long id;
+    private String name;
+    private String nationality;
+
+}
