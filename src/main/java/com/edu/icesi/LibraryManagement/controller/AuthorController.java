a0f45d7949768b03521a53ff84dd963ad9b3c5e6
CarlosJaBr <carjabo12@gmail.com>
2023-10-12 15:18:20 -0500

    feat_add_methods_in_AuthorController

--- src/main/java/com/edu/icesi/LibraryManagement/controller/AuthorController.java
+++ src/main/java/com/edu/icesi/LibraryManagement/controller/AuthorController.java
@@ -1,7 +1,58 @@
 package com.edu.icesi.LibraryManagement.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.edu.icesi.LibraryManagement.persistence.model.Author;
+import com.edu.icesi.LibraryManagement.persistence.model.Book;
+import com.edu.icesi.LibraryManagement.persistence.repository.IBookRepository;
+import com.edu.icesi.LibraryManagement.service.IAuthorService;
+import com.edu.icesi.LibraryManagement.service.IBookService;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/author")
 public class AuthorController {
+
+    private final IAuthorService authorService;
+    private final IBookService bookService;
+
+    public AuthorController(IAuthorService authorService, IBookService bookService) {
+        this.authorService = authorService;
+        this.bookService = bookService;
+    }
+
+    @GetMapping("/autores")
+    public List<Author> showAllAuthor(){
+        return authorService.getAllAuthors();
+    }
+    @GetMapping("/autores/{id}")
+    public Author showAuthor(@PathVariable("id") Long id, Model model){
+        return authorService.findById(id).orElse(null);
+    }
+    @PostMapping("/autores")
+    public Author createAuthor(@RequestBody Author newAuthor){
+        return authorService.saveAuthor(newAuthor);
+    }
+
+    @PutMapping("/autores/{id}")
+    public Boolean uploadAuthor(@PathVariable("id") Long id, Author uploadAuthor){
+        return authorService.uploadAuthor(id, uploadAuthor);
+    }
+
+    @DeleteMapping("/autores/{id}")
+    public Boolean deleteAuthor(@PathVariable("id") Long id){
+        return authorService.deleteAuthor(id);
+    }
+
+    /*
+    @GetMapping("/autores/{id}/libros")
+    public List<Book> showBooksByAuthor(@RequestParam){
+
+    }
+*/
+
+
+
+
 }
--- src/main/java/com/edu/icesi/LibraryManagement/controller/BookController.java
+++ src/main/java/com/edu/icesi/LibraryManagement/controller/BookController.java
@@ -1,7 +1,11 @@
 package com.edu.icesi.LibraryManagement.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/book")
 public class BookController {
+
+
 }
