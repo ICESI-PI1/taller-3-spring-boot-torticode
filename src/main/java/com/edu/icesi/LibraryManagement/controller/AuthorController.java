fe01ee3d455a29feec2fd8beba79c2dabd7b2cae
CarlosJaBr <carjabo12@gmail.com>
2023-10-12 21:12:37 -0500

    feat_edit_methods

--- src/main/java/com/edu/icesi/LibraryManagement/controller/AuthorController.java
+++ src/main/java/com/edu/icesi/LibraryManagement/controller/AuthorController.java
@@ -2,7 +2,6 @@ package com.edu.icesi.LibraryManagement.controller;
 
 import com.edu.icesi.LibraryManagement.persistence.model.Author;
 import com.edu.icesi.LibraryManagement.persistence.model.Book;
-import com.edu.icesi.LibraryManagement.persistence.repository.IBookRepository;
 import com.edu.icesi.LibraryManagement.service.IAuthorService;
 import com.edu.icesi.LibraryManagement.service.IBookService;
 import org.springframework.ui.Model;
@@ -21,13 +20,12 @@ public class AuthorController {
         this.authorService = authorService;
         this.bookService = bookService;
     }
-
     @GetMapping("/autores")
     public List<Author> showAllAuthor(){
         return authorService.getAllAuthors();
     }
     @GetMapping("/autores/{id}")
-    public Author showAuthor(@PathVariable("id") Long id, Model model){
+    public Author showAuthor(@PathVariable Long id, Model model){
         return authorService.findById(id).orElse(null);
     }
     @PostMapping("/autores")
@@ -36,21 +34,19 @@ public class AuthorController {
     }
 
     @PutMapping("/autores/{id}")
-    public Boolean uploadAuthor(@PathVariable("id") Long id, Author uploadAuthor){
+    public Boolean uploadAuthor(@PathVariable Long id, Author uploadAuthor){
         return authorService.uploadAuthor(id, uploadAuthor);
     }
-
     @DeleteMapping("/autores/{id}")
-    public Boolean deleteAuthor(@PathVariable("id") Long id){
+    public Boolean deleteAuthor(@PathVariable Long id){
         return authorService.deleteAuthor(id);
     }
 
-    /*
     @GetMapping("/autores/{id}/libros")
-    public List<Book> showBooksByAuthor(@RequestParam){
-
+    public List<Book> showBooksByAuthor(@PathVariable Long idAuthor){
+        return bookService.getBooksbyAuthor(idAuthor);
     }
-*/
+
 
 
 
--- src/main/java/com/edu/icesi/LibraryManagement/controller/BookController.java
+++ src/main/java/com/edu/icesi/LibraryManagement/controller/BookController.java
@@ -1,11 +1,25 @@
 package com.edu.icesi.LibraryManagement.controller;
 
+import com.edu.icesi.LibraryManagement.persistence.model.Book;
+import com.edu.icesi.LibraryManagement.service.IBookService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/book")
 public class BookController {
 
+    private final IBookService bookService;
+    public BookController(IBookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping("/libros")
+    public List<Book> showAllBooks(){
+        return bookService.getAllBooks();
+    }
 
 }
